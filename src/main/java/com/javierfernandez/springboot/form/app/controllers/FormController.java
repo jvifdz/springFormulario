1d9e79dcad33e5cf01c9cbb9ced165a8ab216bf1
jvifdzbjj-GRANDE <javiason@gmail.com>
2020-10-07 16:47:21 +0200

    añadidos formularios y resultados

--- src/main/java/com/javierfernandez/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/javierfernandez/springboot/form/app/controllers/FormController.java
@@ -4,20 +4,33 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FormController {
 
     @GetMapping("/form")
     public String form(Model model){
+
+        model.addAttribute("titulo", "Formulario usuarios");
         return "form";
     }
 
     @PostMapping("/form")
-    public String procesar(Model model){
+    public String procesar(Model model,
+                           @RequestParam(name = "username") String username,
+                           @RequestParam String password,
+                           @RequestParam String email){
+        model.addAttribute("titulo", "Resultado form");
+
+        model.addAttribute("username", username);
+        model.addAttribute("password", password);
+        model.addAttribute("email", email);
+
+
         return "resultado";
     }
 
-    
+
 
 }
