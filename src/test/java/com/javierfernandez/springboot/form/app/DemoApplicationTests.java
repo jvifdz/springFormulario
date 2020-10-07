a184329c9b251ecf8d992e0a926b4aa62947c861
jvifdzbjj-GRANDE <javiason@gmail.com>
2020-10-07 16:36:59 +0200

    Initial commit

---
+++ src/test/java/com/javierfernandez/springboot/form/app/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javierfernandez.springboot.form.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
