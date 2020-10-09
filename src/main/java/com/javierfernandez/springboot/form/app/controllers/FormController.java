af5528ee88e3fb1af179861d46339b973173e5ed
jvifdzbjj-GRANDE <javiason@gmail.com>
2020-10-09 12:16:45 +0200

    cambio a user en el nombre de la vista

--- src/main/java/com/javierfernandez/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/javierfernandez/springboot/form/app/controllers/FormController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -20,12 +21,14 @@ public class FormController {
 
         Usuario usuario = new Usuario();
         model.addAttribute("titulo", "Formulario usuarios");
-        model.addAttribute("usuario", usuario);
+        /*seria usuario pero he puesto en el model attribute user para cambiar*/
+        model.addAttribute("user" , usuario);
         return "form";
     }
 
     @PostMapping("/form")
-    public String procesar(@Valid Usuario usuario, BindingResult result, Model model
+    /*para poner nombre distinto en la vista moddel attribute*/
+    public String procesar(@Valid @ModelAttribute("user") Usuario usuario, BindingResult result, Model model
 
                            //no hace falta ya que mapeamos desde el objeto
                            /*@RequestParam(name = "username") String username,
