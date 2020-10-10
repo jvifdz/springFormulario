aaa65662efbcbc5b53caf1ac73f6e4f3294624c6
jvifdzbjj-GRANDE <javiason@gmail.com>
2020-10-10 21:08:10 +0200

    th field para ya no poner la clase solo el atributo del objeto y errores manejados por spring con fields has errors

--- src/main/java/com/javierfernandez/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/javierfernandez/springboot/form/app/controllers/FormController.java
@@ -38,12 +38,15 @@ public class FormController {
         model.addAttribute("titulo", "Resultado form");
 
         if (result.hasErrors()) {
-            Map<String, String> errores = new HashMap<>();
+            //Error de forma manual
+           /* Map<String, String> errores = new HashMap<>();
             result.getFieldErrors().forEach(err -> {
                 errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
             });
 
-            model.addAttribute("error", errores);
+            model.addAttribute("error", errores);*/
+
+
 
             return "form";
         }
