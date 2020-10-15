b2bf4094041709bd5883b0f2a54396a9ece13ca9
jvifdzbjj-GRANDE <javiason@gmail.com>
2020-10-15 18:20:07 +0200

    formateo de fecha con el binder y metido en el metodo el campo espcifico de fecha

--- src/main/java/com/javierfernandez/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/javierfernandez/springboot/form/app/controllers/FormController.java
@@ -29,7 +29,7 @@ public class FormController {
 
         SimpleDateFormat dateFormat= new SimpleDateFormat("yyyy-MM-dd");
         dateFormat.setLenient(false);
-        binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, false));
+        binder.registerCustomEditor(Date.class,"fechaNacimiento" ,new CustomDateEditor(dateFormat, true));
 
     }
 
