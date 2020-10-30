3bb4ea604b0bae4a1aae8b90b24a8ecb68ec7326
jvifdz-PORTATIL <javiason@gmail.com>
2020-10-30 14:26:16 +0100

    Añadido paquete interceptors

---
+++ src/main/java/com/javierfernandez/springboot/form/app/interceptors/TiempoTranscurridoInterceptor.java
@@ -0,0 +1,21 @@
+package com.javierfernandez.springboot.form.app.interceptors;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class TiempoTranscurridoInterceptor  implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+}
